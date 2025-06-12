a0de61482bd2e56e85f5c2b2afcb4d4ce940a45f
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2025-06-12 18:08:08 +0600

    set up

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const mongoose = require("mongoose");
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
