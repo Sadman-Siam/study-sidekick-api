48034e366a8668e77d8698056b0f7b0ff7240224
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2025-06-12 18:25:29 +0600

    set up

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 5000;
 
 const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
